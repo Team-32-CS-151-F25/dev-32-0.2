687ad55c9611938c4cc7d1bcaddf4049b599210e
bhusalsa <156117237+bhusalsa@users.noreply.github.com>
2025-10-04 09:21:34 -0700

    Added the ProgrammingLanguage class to add, get the languages

---
+++ src/main/java/cs151/application/ProgrammingLanguage.java
@@ -0,0 +1,17 @@
+package cs151.application;
+
+import java.io.IOException;
+
+public class ProgrammingLanguage {
+    private final static String filename = "src/main/resources/cs151/application/languages.csv";
+    private static CSVParser parser = new CSVParser(filename);
+
+
+    public void setLanguage(String language) throws IOException {
+        parser.setData(language);
+    }
+
+    public String getLanguage(String language) {
+        return parser.getData();
+    }
+}
