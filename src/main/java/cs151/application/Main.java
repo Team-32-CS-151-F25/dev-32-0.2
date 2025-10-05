3eebd4d1457aaa002b84eee96e5b4d52b8ce2633
LJennifer21 <lucerojennifer607@gmail.com>
2025-10-04 17:11:36 -0700

    Changes to UI

--- src/main/java/cs151/application/Main.java
+++ src/main/java/cs151/application/Main.java
@@ -3,7 +3,9 @@ package cs151.application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -11,10 +13,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
