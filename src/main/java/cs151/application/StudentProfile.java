60e0b4c3d1e6c913dd0af1bc1c4fa07f86564042
bhusalsa <156117237+bhusalsa@users.noreply.github.com>
2025-10-13 16:23:15 -0700

    Setup Class for Student Profiles

---
+++ src/main/java/cs151/application/StudentProfile.java
@@ -0,0 +1,11 @@
+package cs151.application;
+
+public class StudentProfile {
+    private final static String filename = "src/main/resources/cs151/application/studentsProfile.csv";
+
+    // Name
+    // Academic Status - dropdown list , hard coded
+    // Job Status - employed/ unemployed
+    // Job Details - required if employed
+
+}
